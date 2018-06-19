7f0a6c7ae63fc4a0e6171a6af2f243a6aa6e73b8
felipera <filipinas.3@gmail.com>
2018-06-19 20:41:39 -0300

    tb instfetch

--- src/tbs/InstFetch_tb.v
+++ src/tbs/InstFetch_tb.v
@@ -14,15 +14,15 @@ reg [31:0] MEM_OUT;
 
 InstFetch tb_inst
 (
-  .MEM_CLK(MEM_CLK),
-  .NPC(NPC),
-  .IR(IR),
-  .ESTADO(ESTADO),
-  .COND(COND),
-  .CLK(CLK),
-  .RST(RST),
-  .ULA(ULA),
-  .MEM_OUT(MEM_OUT)
+	.MEM_CLK(MEM_CLK),
+	.NPC(NPC),
+	.IR(IR),
+	.ESTADO(ESTADO),
+	.COND(COND),
+	.CLK(CLK),
+	.RST(RST),
+	.ULA(ULA),
+	.MEM_OUT(MEM_OUT)
 );
 
 always #20 CLK = ~CLK;
@@ -33,29 +33,118 @@ begin
 
 $monitor("TEMPO:%d -- MEM_CLK:%d | NPC:%d | MEM_ADDR:%d | IR:%d | ESTADO:%d",
 	$time, MEM_CLK, NPC, IR, ESTADO);
-	 // estados iniciais
-	 CLK = 0;
-	 RST = 0;
+	// estados iniciais
+	CLK = 0;
+	RST = 0;
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 0;
 
-	 // ---- INICIO ----
-	 #20 // clock 1
-	 RST = 1; // retira o reset
-	 
-	 #40 // clock 2
+	// ---- INICIO ----
+	#20 // clock 1
+	RST = 1; // retira o reset
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
 
-	 #41 // clock 3
+	#40 // clock 2
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
 
-	 #41 // clock 4
+	#41 // clock 3
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
 
-	 #41 // clock 5
+	#41 // clock 4
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
 
-	 #41 // clock 6
+	#41 // clock 5
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
 
-	 #41 // clock 7
+	#41 // clock 6
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
 
-	 // etc ...
+	#41 // clock 7
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
 
+	// etc ...
 
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	#41
+	COND = 0;
+	ULA = 0;
+	MEM_OUT = 16'b1111111111111111;
+
+	
 	 #2000 $stop;
 end
 
